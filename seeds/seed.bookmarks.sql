c8affbc3143896dba578fd90b43633d6c79c4a12
Christopher Williams <cw@chriswillia.ms>
2019-08-07 11:17:54 -0500

    Resolves a bug with seed.bookmarks script.

--- seeds/seed.bookmarks.sql
+++ seeds/seed.bookmarks.sql
@@ -1,7 +1,7 @@
 insert into bookmarks (title, url, description, rating)
 values
-('Google', 'http://google.com', 'The Google homepage', 3),
-('Amazon', 'https://amazon.com', NULL, 4),
-('My portfolio', 'https://christopherwilliams.dev', 'Amazing, yeah?', 5),
-('Thinkful', 'https://thinkful.com', NULL, 5),
-('React', 'https://reactjs.org', 'The React site', 4)
+('Google', 'http://google.com', 'The Google homepage', '3'),
+('Amazon', 'https://amazon.com', NULL, '4'),
+('My portfolio', 'https://christopherwilliams.dev', 'Amazing, yeah?', '5'),
+('Thinkful', 'https://thinkful.com', NULL, '5'),
+('React', 'https://reactjs.org', 'The React site', '4')
