4628162c8bfc43110dd5d7231df2e597cc6b2092
Christopher Williams <cw@chriswillia.ms>
2019-08-22 15:02:54 -0500

    Revert "Testing"
    This reverts commit d1087726d1e59a738d5cdb86f3c9d183f57bb38e.

--- src/app.js
+++ src/app.js
@@ -16,15 +16,15 @@ app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
 
-// app.use(function validateBearerToken(req, res, next) {
-//     const apiToken = process.env.API_TOKEN;
-//     const authToken = req.get('Authorization');
-//     if(!authToken || authToken.split(' ')[1] !== apiToken) {
-//         logger.error(`Invalid API key in request authorization header`);
-//         return res.status(400).json({error: 'Unauthorized request'});
-//     }
-//     next();
-// });
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Invalid API key in request authorization header`);
+        return res.status(400).json({error: 'Unauthorized request'});
+    }
+    next();
+});
 
 app.use('/api/bookmarks', bookmarksRouter);
 
