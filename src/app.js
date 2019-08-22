e683f7dbf4c120d583c7a2636651b025d3b31b39
Christopher Williams <cw@chriswillia.ms>
2019-08-22 14:17:22 -0500

    Adds winston logging for errors in app.js

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const logger = require('./logger');
 const { NODE_ENV } = require('./config');
 const bookmarksRouter = require('./bookmarks/bookmark-route');
 
@@ -19,6 +20,7 @@ app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN;
     const authToken = req.get('Authorization');
     if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Invalid API key in request authorization header`);
         return res.status(400).json({error: 'Unauthorized request'});
     }
     next();
@@ -33,6 +35,7 @@ app.use(function errorHandler(error, req, res, next) {
     } else {
         response = { error };
     };
+    logger.error(error);
     res.status(500).json(response);
 });
 
